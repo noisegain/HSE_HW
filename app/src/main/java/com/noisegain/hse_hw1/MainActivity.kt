76f93cd41a851719f5063ef237f4d806cb0f6ec5
ispon <is.ponomarenko222q@gmail.com>
2021-07-23 23:39:49 +0700

    some changes

--- app/src/main/java/com/noisegain/hse_hw1/MainActivity.kt
+++ app/src/main/java/com/noisegain/hse_hw1/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.noisegain.hse_hw1
 
-import android.R
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +10,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    // Не успел доделать через recyclerView :(
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -19,7 +20,7 @@ class MainActivity : AppCompatActivity() {
             title = "О себе"
             description.text = "Приложение для чтения новостей\nГруппировка по интересам, выбор источников, тем"
             gitButton.setOnClickListener {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com")))
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/noisegain")))
             }
         }
     }
